e38c52c3b3a1777c94708ce95968b93cc6137d39
davewaldorf <dave.waldorf@me.com>
2023-10-12 01:23:01 +0200

    feat: Add Express session authentication
    Added session-based authentication to the Express server. A unique session ID is generated and stored in cookies for each user. This session ID is checked to authenticate users and identify them. The session expires after 1 hour,

--- server/server.js
+++ server/server.js
@@ -2,23 +2,49 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const routes = require('./routes');
+const session = require('express-session');
+const { v4: uuidv4 } = require('uuid');
 
+const app = express();
+const port = 4000;
+
+// Configure CORS
 const corsConfig = {
   origin: true,
   credentials: true,
 };
 
-const app = express();
 // Enable cross-origin resource sharing
 app.use(cors(corsConfig));
+
 // Parse request bodies as JSON
 app.use(bodyParser.json());
+
+app.use(session({
+  genid: (req) => {
+    console.log('Inside the session middleware');
+    console.log(req.sessionID);
+    return uuidv4(); // use UUIDs for session IDs
+  },
+  sameSite: true,
+  store: new session.MemoryStore(),
+  secret: 'carbon',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 3600000 } // session expires after 1 hour (in milliseconds)
+}));
+
+app.use((req, res, next) => {
+  res.cookie('sid', req.sessionID, {
+    maxAge: 3600000,
+  });
+  next();
+})
 
 // Mount routes
 app.use(routes);
 
 // Start the server
-const port = 4000;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
